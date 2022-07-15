430d7d1aff8759817d906e11a57e88c95b503ab6
DaddarioLuigi <44373889+DaddarioLuigi@users.noreply.github.com>
2022-07-15 19:18:05 +0200

    sendmail.php

---
+++ sendMail.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_POST['contactForm']))
+{
+
+	$to = 'l.daddario@studenti.uniba.it';
+	$subject = 'Messaggio dal sito web';
+	$message = 'hello';
+	$headers = 'From: l.daddario@studenti.uniba.it' . "\r\n" .
+	    'Reply-To: l.daddario@studenti.uniba.it' . "\r\n" .
+	    'X-Mailer: PHP/' . phpversion();
+
+	mail($to, $subject, $message, $headers);
+
+}
+?>
