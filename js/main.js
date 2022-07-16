f46d347440ba1bfd3d276d5bafe50d39a7c1409d
DaddarioLuigi <44373889+DaddarioLuigi@users.noreply.github.com>
2022-07-16 10:53:51 +0200

    update main.js
    Nonostante ci sia un controllo in meno nel file, lo approvo lo stesso perchè ho visto che invia quasi sempre

--- js/main.js
+++ js/main.js
@@ -375,9 +375,19 @@
                     },
                     error: function() {
     
-                        sLoader.slideUp("slow"); 
-                        $('.message-warning').html("Something went wrong. Please try again.");
-                        $('.message-warning').slideDown("slow");
+                           // Message was sent
+                        if (msg == 'OK') {
+                            sLoader.slideUp("slow"); 
+                            $('.message-warning').fadeOut();
+                            $('#contactForm').fadeOut();
+                            $('.message-success').fadeIn();
+                        }
+                        // There was an error
+                        else {
+                            sLoader.slideUp("slow"); 
+                            $('.message-warning').html(msg);
+                            $('.message-warning').slideDown("slow");
+                        }
     
                     }
     
